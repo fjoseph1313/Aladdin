f3cbe0bc15007756bc9e951a84077d2bd83730a0
atan <atan@10.10.53.9>
2014-12-30 23:00:41 -0600

    model classes

--- src/main/java/aladdin/com/model/Person.java
+++ src/main/java/aladdin/com/model/Person.java
@@ -1,7 +1,13 @@
 package aladdin.com.model;
 
 public class Person {
+
 	private String firstName;
+	private String lastName;
+	private Date dateOfBirth;
+	private String phoneNumber;
+	private String emailAddress;
+	private String password;
 
 	public String getFirstName() {
 		return firstName;
@@ -10,6 +16,45 @@ public class Person {
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
-	
-	
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 }
