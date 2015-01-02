47506a4bf667ce03bc91533ebeac13fe2fc0adae
sadakul <sadakbd@gmail.com>
2015-01-01 22:37:34 -0600

    Admin

---
+++ src/main/java/aladdin/com/model/Admin.java
@@ -0,0 +1,31 @@
+package aladdin.com.model;
+
+import javax.persistence.Entity;
+import javax.xml.bind.annotation.XmlRootElement;
+
+
+@XmlRootElement
+@Entity(name = "admin")
+public class Admin {
+	
+	private String userName;
+	private String password;
+	
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+
+}
