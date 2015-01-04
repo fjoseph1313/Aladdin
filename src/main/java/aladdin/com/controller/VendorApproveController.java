bd62b7c87fbabdd46371a5bf332e186bec462752
jcabriana1976 <jcabriana@gmail.com>
2015-01-03 23:47:57 -0600

    email system to vendor

--- src/main/java/aladdin/com/controller/VendorApproveController.java
+++ src/main/java/aladdin/com/controller/VendorApproveController.java
@@ -2,17 +2,17 @@ package aladdin.com.controller;
 
 import java.util.List;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
-
-
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import aladdin.com.MailMail;
 import aladdin.com.dao.DAOFactory;
 import aladdin.com.dao.VendorDAO;
-import aladdin.com.model.ProductCategory;
 import aladdin.com.model.Vendor;
 
 @Controller
@@ -37,6 +37,20 @@ public class VendorApproveController {
 		vDao.editVendor(id);
 		List<Vendor> list= vDao.getVendorForApproval(false);
 		model.addAttribute("lists", list);
+		
+		Vendor vendor = vDao.findByPrimaryKey(id);
+		
+		String fullName = vendor.getFirstName() + " " + vendor.getLastName();
+		String emailAddress = vendor.getEmailAddress();
+		
+		String emailSubject = "Account Activated";
+		String emailBody = "Welcome " + fullName + ",\n\n"
+				+ "Your account is already activated to Aladdin.\n\n";
+				
+		ApplicationContext context = new ClassPathXmlApplicationContext("Spring-Mail.xml");
+    	MailMail mm = (MailMail) context.getBean("mailMail");
+    	mm.sendMail("Aladdin <aladdin.mscs@gmail.com>", emailAddress, "aladdin.mscs@gmail.com", emailSubject, emailBody);
+    	
 		vDao.commitTransaction();
 		return "redirect:/vendor/";
 	}
