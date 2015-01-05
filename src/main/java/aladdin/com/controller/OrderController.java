a30cfa3b594ccbc0d4efdee15cdec51c3784c148
fjoseph1313 <cesc.joseph@gmail.com>
2015-01-04 22:29:09 -0600

    ordercontroller

---
+++ src/main/java/aladdin/com/controller/OrderController.java
@@ -0,0 +1,60 @@
+package aladdin.com.controller;
+
+
+import java.util.Date;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import aladdin.com.dao.*;
+import aladdin.com.model.*;
+
+public class OrderController {
+	
+	private DAOFactory daoFactory = DAOFactory.getFactory();
+	OrderDAO orderDao = daoFactory.getOrderDAO();
+	ProductDAO productDao = daoFactory.getProductDAO();
+	CartDAO cartDao = daoFactory.getCartDAO();
+	
+	@RequestMapping(value = "/order/{id}")
+	public String createOrder(@PathVariable Long id, Cart cart)
+	{
+		//get current logged in customer and create new order and find if its order still exists, else create new
+		Long custId = new Long(1);
+		String result = "index";
+		orderDao.beginTransaction();
+		Order existingOrder = orderDao.findByCustomerIdAndStatus(custId);
+		Product fetchedProduct = (Product) productDao.findByPrimaryKey(id);
+		
+		if(existingOrder != null)
+		{
+			cart.setProduct(fetchedProduct);
+			cart.setOrder(existingOrder);
+			existingOrder.getCart().add(cart); //just for bidirectional association.
+			cartDao.save(cart);
+			orderDao.save(existingOrder); //persisting an updated order
+			orderDao.commitTransaction();
+			
+			result = "viewOrder";
+		}
+		else{	
+			Order order = new Order();
+			order.setOrderCreateDate(new Date());
+			order.setOrderAmount(fetchedProduct.getPrice() * cart.getQuantity());
+			order.setQuantity(cart.getQuantity());
+			//order.setCustomer(loggedInUser);
+			order.setOrderStatus("new"); //assumption, on first creation, set status new.
+			
+			cart.setProduct(fetchedProduct);
+			cart.setOrder(order);
+			cartDao.save(cart);
+			orderDao.save(order);
+			
+			result = "viewOrder";
+		}
+		
+		
+		return result;
+	}
+
+}
--- src/main/java/aladdin/com/dao/OrderDAOImpl.java
+++ src/main/java/aladdin/com/dao/OrderDAOImpl.java
@@ -1,5 +1,8 @@
 package aladdin.com.dao;
 
+import javax.persistence.Query;
+
+import aladdin.com.HibernateUtil;
 import aladdin.com.model.Order;
 
 public class OrderDAOImpl extends HibernateDAO<Order, Long> implements OrderDAO
@@ -7,5 +10,12 @@ public class OrderDAOImpl extends HibernateDAO<Order, Long> implements OrderDAO
 	public OrderDAOImpl()
 	{
 		super(Order.class);
+	}
+	
+	public Order findByCustomerIdAndStatus(Long custid)
+	{
+		Query query = (Query) HibernateUtil.getSession().createQuery("FROM Order or WHERE or.customer = :id and or.orderStatus = 'new'");
+		query.setParameter("id", custid);
+		return (Order) query.getSingleResult();
 	}
 }
